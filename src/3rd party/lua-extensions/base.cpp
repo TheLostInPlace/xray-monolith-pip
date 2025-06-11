7d77953e8076b1a171d7bd39e35b17b9fd87893f
SaloEater <danil.lazarev@gmail.com>
2025-06-10 22:31:51 -0500

    revert luajit lib for luaext

--- src/3rd party/lua-extensions/base.cpp
+++ src/3rd party/lua-extensions/base.cpp
@@ -3,7 +3,7 @@
 #include "luapanda.h"
 
 #ifdef USE_LUAJIT_ONE
-#pragma comment(lib, "LuaJIT-1.1.4.lib")
+#pragma comment(lib, "LuaJIT-1.1.8.lib")
 #else
 #pragma comment(lib, "lua51.lib")
 #endif //-USE_LUAJIT_ONE
