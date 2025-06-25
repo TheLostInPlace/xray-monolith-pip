ba459af68a7935987f03f64575f3dc4a700304db
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-06-25 22:41:05 +1000

    commit: 8c2c3f3a4604e7076082fb0cc1e60ac92ea8921d particle_manager, xrMemory shared pointers
    Co-authored-by: ForserX <sv3nk@yandex.ru>

--- src/xrCore/xrMemory_subst_msvc.h
+++ src/xrCore/xrMemory_subst_msvc.h
@@ -200,3 +200,29 @@ void XRCORE_API mem_alloc_gather_stats_frequency(const float& value);
 void XRCORE_API mem_alloc_show_stats();
 void XRCORE_API mem_alloc_clear_stats();
 #endif // DEBUG_MEMORY_MANAGER
+
+#include <memory>
+
+template<typename T>
+using xr_shared_ptr = std::shared_ptr<T>;
+
+template<typename T>
+using xr_unique_ptr = std::unique_ptr<T, xr_special_free<false, T>>;
+
+template <class T, class... Args>
+xr_shared_ptr<T> xr_make_shared(Args&&... args)
+{
+	return xr_shared_ptr<T>(new T(std::forward<Args>(args)...), [](T* ptr)
+		{
+			xr_special_free<false, T> deleter;
+			deleter(ptr);
+		});
+}
+
+template <typename T, typename... ARGS>
+xr_unique_ptr<T> xr_make_unique(ARGS&&... args)
+{
+	void* TypeMem = Memory.mem_alloc(sizeof(T));
+	new (TypeMem)T(std::forward<ARGS>(args)...);
+	return xr_unique_ptr<T>(reinterpret_cast<T*>(TypeMem), xr_special_free<false, T>{});
+}
--- src/xrParticles/particle_manager.h
+++ src/xrParticles/particle_manager.h
@@ -3,52 +3,63 @@
 #define particle_managerH
 //---------------------------------------------------------------------------
 #include "particle_actions.h"
+#include <atomic>
 
 namespace PAPI
 {
-	class CParticleManager : public IParticleManager
-	{
-		// These are static because all threads access the same effects.
-		// All accesses to these should be locked.
-		DEFINE_VECTOR(ParticleEffect*, ParticleEffectVec, ParticleEffectVecIt);
-		DEFINE_VECTOR(ParticleActions*, ParticleActionsVec, ParticleActionsVecIt);
-		ParticleEffectVec effect_vec;
-		ParticleActionsVec m_alist_vec;
-	public:
-		CParticleManager();
-		virtual ~CParticleManager();
-		// Return an index into the list of particle effects where
-		ParticleEffect* GetEffectPtr(int effect_id);
-		ParticleActions* GetActionListPtr(int alist_id);
+    class CParticleManager :
+        public IParticleManager,
+        public pureFrame
+    {
+        // These are static because all threads access the same effects.
+        // All accesses to these should be locked.
 
-		// create&destroy
-		virtual int CreateEffect(u32 max_particles);
-		virtual void DestroyEffect(int effect_id);
-		virtual int CreateActionList();
-		virtual void DestroyActionList(int alist_id);
+        using SharedParticleEffect = xr_shared_ptr<ParticleEffect>;
+        using SharedParticleActions = xr_shared_ptr<ParticleActions>;
 
-		// control
-		virtual void PlayEffect(int effect_id, int alist_id);
-		virtual void StopEffect(int effect_id, int alist_id, BOOL deffered = TRUE);
+        using ParticleEffectVec = xr_hash_map<int, SharedParticleEffect>;
+        using ParticleActionsVec = xr_hash_map<int, SharedParticleActions>;
+        ParticleEffectVec m_effect_map;
+        ParticleActionsVec m_alist_map;
+        std::atomic_int m_effect_counter;
+        std::atomic_int m_action_counter;
 
-		// update&render
-		virtual void Update(int effect_id, int alist_id, float dt);
-		virtual void Render(int effect_id);
-		virtual void Transform(int alist_id, const Fmatrix& m, const Fvector& velocity);
+        xrCriticalSection m_effect_guard;
+        xrCriticalSection m_action_guard;
 
-		// effect
-		virtual void RemoveParticle(int effect_id, u32 p_id);
-		virtual void SetMaxParticles(int effect_id, u32 max_particles);
-		virtual void SetCallback(int effect_id, OnBirthParticleCB b, OnDeadParticleCB d, void* owner, u32 param);
-		virtual void GetParticles(int effect_id, Particle*& particles, u32& cnt);
-		virtual u32 GetParticlesCount(int effect_id);
+    public:
+        CParticleManager();
+        virtual						~CParticleManager();
+        // Return an index into the list of particle effects where
+        SharedParticleEffect		GetEffectPtr(int effect_id);
+        SharedParticleActions		GetActionListPtr(int alist_id);
 
-		// action
-		virtual ParticleAction* CreateAction(PActionEnum action_id);
-		virtual u32 LoadActions(int alist_id, IReader& R);
-		virtual void SaveActions(int alist_id, IWriter& W);
-	};
+        // create&destroy
+        virtual int					CreateEffect(u32 max_particles);
+        virtual void				DestroyEffect(int effect_id);
+        virtual int					CreateActionList();
+        virtual void				DestroyActionList(int alist_id);
+
+        // control
+        virtual void				PlayEffect(int effect_id, int alist_id);
+        virtual void				StopEffect(int effect_id, int alist_id, BOOL deffered = TRUE);
+
+        // update&render
+        virtual void				Update(int effect_id, int alist_id, float dt);
+        virtual void				Transform(int alist_id, const Fmatrix& m, const Fvector& velocity);
+        // effect
+        virtual void				RemoveParticle(int effect_id, u32 p_id);
+        virtual void				SetMaxParticles(int effect_id, u32 max_particles);
+        virtual void				SetCallback(int effect_id, OnBirthParticleCB b, OnDeadParticleCB d, void* owner, u32 param);
+        virtual void				GetParticles(int effect_id, Particle*& particles, u32& cnt);
+        virtual u32					GetParticlesCount(int effect_id);
+
+        // action
+        virtual ParticleAction* CreateAction(PActionEnum action_id);
+        virtual u32					LoadActions(int alist_id, IReader& R);
+        virtual void				SaveActions(int alist_id, IWriter& W);
+        virtual void                OnFrame() override;
+    };
 };
-
 //---------------------------------------------------------------------------
 #endif
