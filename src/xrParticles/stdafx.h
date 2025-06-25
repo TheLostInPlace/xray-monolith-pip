ae977be48f16c784181dcb0d09ac56e57dc57ee6
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-06-25 21:57:43 +1000

    commit: 8c2c3f3a4604e7076082fb0cc1e60ac92ea8921d xrParticles/stdafx changed xrParticls project to include luabind
    Co-authored-by: ForserX <sv3nk@yandex.ru>

--- src/xrParticles/stdafx.h
+++ src/xrParticles/stdafx.h
@@ -9,14 +9,7 @@
 
 #pragma once
 
-#include "../xrCore/xrCore.h"
-
+#include "../xrEngine/stdafx.h"
 #include "psystem.h"
-
-#ifdef __BORLANDC__
-#pragma comment( lib, "x:\\xrCoreB.lib"	)
-#else
-#pragma comment( lib, "xrCore.lib"	)
-#endif
 
 #endif //stdafxH
