5e92e8b4e1d2c607ad522ee80fba5152e5ea8db6
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-07-15 05:22:02 +1000

    restore comments

--- src/xrSound/SoundRender_Emitter_FSM.cpp
+++ src/xrSound/SoundRender_Emitter_FSM.cpp
@@ -16,7 +16,7 @@ XRSOUND_API extern float psSoundCull;
 inline u32 calc_cursor(const float& fTimeStarted, float& fTime, const float& fTimeTotal, const WAVEFORMATEX& wfx)
 {
 	if (fTime < fTimeStarted)
-		fTime = fTimeStarted; // ������� �����������, ������ ��� ���� ������� �� �� ����� ��� �� �����
+		fTime = fTimeStarted; // Андрюха посоветовал, ассерт что ниже вылетел из за паузы как то хитро
 	R_ASSERT((fTime-fTimeStarted)>=0.0f);
 	while ((fTime - fTimeStarted) > fTimeTotal) //looped
 	{
