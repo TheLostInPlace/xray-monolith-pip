8526e9428b14bddaeef6e2af3c9afb2a8c430a25
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-08-08 03:35:00 +0100

    Deduplicate grenade launching logic into `CWeaponGrenadeLauncher`

---
+++ src/xrGame/WeaponGrenadeLauncher.h
@@ -0,0 +1,10 @@
+#ifndef _WEAPON_GRENADE_LAUNCHER_
+#define _WEAPON_GRENADE_LAUNCHER_
+
+class CWeaponGrenadeLauncher
+{
+public:
+    static void LaunchGrenade(CWeapon* wpn);
+};
+
+#endif //_WEAPON_GRENADE_LAUNCHER
