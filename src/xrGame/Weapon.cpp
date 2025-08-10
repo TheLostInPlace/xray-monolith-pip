bb35542fba9a299137aff0d6680f4b0d89854793
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-08-10 10:20:26 +1000

    g_interrupt_fire_on_aim_toggle cvar to set stop firing when pressed aim
    (cherry picked from commit 4b9df8f052fd10f1fd2c113bdd9abbd0dfd5cf0e)

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -1440,6 +1440,7 @@ void CWeapon::UpdatePosition(const Fmatrix& trans)
 	VERIFY(!fis_zero(DET(renderable.xform)));
 }
 
+BOOL interruptFireOnAimToggle = FALSE;
 bool CWeapon::Action(u16 cmd, u32 flags)
 {
 	if (inherited::Action(cmd, flags)) return true;
@@ -1495,7 +1496,7 @@ bool CWeapon::Action(u16 cmd, u32 flags)
 
 							if (GetState() != eAimStart && HudAnimationExist("anm_idle_aim_start"))
 								SwitchState(eAimStart);
-							else if (GetState() != eIdle)
+							else if (interruptFireOnAimToggle && GetState() != eIdle)
 								SwitchState(eIdle);
 
 							OnZoomIn();
@@ -1521,7 +1522,7 @@ bool CWeapon::Action(u16 cmd, u32 flags)
 
 						if (GetState() != eAimStart && HudAnimationExist("anm_idle_aim_start"))
 							SwitchState(eAimStart);
-						else if (GetState() != eIdle)
+						else if (interruptFireOnAimToggle && GetState() != eIdle)
 							SwitchState(eIdle);
 
 						OnZoomIn();
