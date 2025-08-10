99fd1b8e79f08f61420304f89916df5ecb3a1925
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-08-11 02:13:26 +1000

    define xr_hash, use robin hood hash if toggled, define xr_hash_map as robinhood unordered map

--- src/xrGame/xrServer_process_spawn.cpp
+++ src/xrGame/xrServer_process_spawn.cpp
@@ -79,7 +79,7 @@ CSE_Abstract* xrServer::Process_spawn(NET_Packet& P, ClientID sender, BOOL bSpaw
 		Phantom->ID = PerformIDgen(0xffff);
 		Phantom->ID_Phantom = Phantom->ID; // Self-linked to avoid phantom-breeding
 		Phantom->owner = NULL;
-		entities.insert(mk_pair(Phantom->ID, Phantom));
+		entities.insert({ Phantom->ID, Phantom });
 
 		Phantom->s_flags.set(M_SPAWN_OBJECT_PHANTOM,TRUE);
 
@@ -87,7 +87,7 @@ CSE_Abstract* xrServer::Process_spawn(NET_Packet& P, ClientID sender, BOOL bSpaw
 		E->ID = PerformIDgen(E->ID);
 		E->ID_Phantom = Phantom->ID;
 		E->owner = CL;
-		entities.insert(mk_pair(E->ID, E));
+		entities.insert({ E->ID, E });
 	}
 	else
 	{
@@ -97,7 +97,7 @@ CSE_Abstract* xrServer::Process_spawn(NET_Packet& P, ClientID sender, BOOL bSpaw
 			E->ID = PerformIDgen(0xffff);
 			E->owner = CL; //		= SelectBestClientToMigrateTo	(E);
 			E->s_flags.set(M_SPAWN_OBJECT_PHANTOM,FALSE);
-			entities.insert(mk_pair(E->ID, E));
+			entities.insert({ E->ID, E });
 		}
 		else
 		{
@@ -111,7 +111,7 @@ CSE_Abstract* xrServer::Process_spawn(NET_Packet& P, ClientID sender, BOOL bSpaw
 			}
 			E->ID = PerformIDgen(E->ID);
 			E->owner = CL;
-			entities.insert(mk_pair(E->ID, E));
+			entities.insert({ E->ID, E });
 		}
 	}
 
