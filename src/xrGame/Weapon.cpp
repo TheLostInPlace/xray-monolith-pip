7cbb6017ffb0401956330253621c7fd79b2ac467
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-08-12 23:39:24 +1000

    // v2v3v4: fix ctd when into about to be destroyed object with detector scopes

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -3295,3 +3295,14 @@ Fmatrix CWeapon::RayTransform()
 
 	return matrix;
 }
+
+// v2v3v4: fix ctd when into about to be destroyed object with detector scopes
+void CWeapon::net_Relcase(CObject* object)
+{
+	CHudItem::net_Relcase(object);
+
+	if (!m_zoom_params.m_pVision)
+		return;
+
+	m_zoom_params.m_pVision->remove_links(object);
+}
