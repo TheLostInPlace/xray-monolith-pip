de51506cee412d99171269eb531e8468f6c77551
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-08-16 03:43:07 +1000

    hudmanager fix
    (cherry picked from commit f8e5b7a7ff5224d82514ad8602fddef6b660475d)

--- src/xrGame/HUDManager.cpp
+++ src/xrGame/HUDManager.cpp
@@ -177,6 +177,7 @@ void CHUDManager::OnFrameMT()
 		Level().GameTaskManager().UpdateTasks();
 
 	PP.CameraPick();
+	g_player_hud->OnFrame();
 	DoPick(PP);
 
 	xrCriticalSectionGuard guard(&ui_lock);
