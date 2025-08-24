2b7e4521a96712cb24c55b38df07952f20ef14da
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-08-24 12:45:53 +1000

    be597f64cab67c91536c60a6cd4b245b3de3b684 Restore mutant capabilities
    Co-authored-by: St4lker0k765 <St4lker0k765@users.noreply.github.com>

--- src/Layers/xrRender/SkeletonAnimated.h
+++ src/Layers/xrRender/SkeletonAnimated.h
@@ -177,7 +177,8 @@ public:
 	MotionID ID_FX(LPCSTR N);
 	MotionID ID_FX_Safe(LPCSTR N);
 	CBlend* PlayFX(LPCSTR N, float power_scale);
-	CBlend* PlayFX(MotionID M, float power_scale);
+	CBlend* PlayFX(MotionID M, float power_scale) override;
+	CBlend* PlayFX_Safe (LPCSTR N, float power_scale) override;
 
 	const CPartition& partitions() const { return *m_Partition; };
 
