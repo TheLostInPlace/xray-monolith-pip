28dea9b575c2dfdd237733cd5a17f964a0eab676
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2026-02-24 11:58:45 +1000

    fix repeated load of game_tutorials.xml, fixes campfire stutter

--- src/xrGame/ui/UIGameTutorial.cpp
+++ src/xrGame/ui/UIGameTutorial.cpp
@@ -109,8 +109,18 @@ void CUISequencer::Start(LPCSTR tutor_name)
 
 	m_name = tutor_name;
 
-	CUIXml uiXml;
-	uiXml.Load(CONFIG_PATH, UI_PATH, "game_tutorials.xml");
+    static CUIXml uiXml;
+    static bool uiXmlLoaded;
+    if (uiXmlLoaded)
+    {
+        uiXml.SetLocalRoot(uiXml.GetRoot());
+    }
+    else
+    {
+        uiXmlLoaded = true;
+        uiXml.Load(CONFIG_PATH, UI_PATH, "game_tutorials.xml");
+    }
+        
 
 	int items_count = uiXml.GetNodesNum(tutor_name, 0, "item");
 	VERIFY(items_count>0);
