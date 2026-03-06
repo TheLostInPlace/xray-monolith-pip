4cf422063fe63161fd620b5fd8a6c34d0335b03d
leer-h <157538103+leer-h@users.noreply.github.com>
2026-03-06 18:35:41 +0200

    Added "show_actor_body" command to enable rendering whole body and weapons in FP mode

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch_script.h"
+#include "pch_script.h"
 #include "Actor_Flags.h"
 #include "hudmanager.h"
 #ifdef DEBUG
@@ -110,6 +110,8 @@ static Fbox bbStandBox;
 static Fbox bbCrouchBox;
 static Fvector vFootCenter;
 static Fvector vFootExt;
+
+BOOL showActorBody = FALSE;
 
 Flags32 psActorFlags = {AF_GODMODE_RT | AF_AUTOPICKUP | AF_RUN_BACKWARD | AF_IMPORTANT_SAVE | AF_USE_TRACERS};
 int psActorSleepTime = 1;
@@ -2136,10 +2138,16 @@ void CActor::renderable_Render()
 	{
 		if (::Render->active_phase() == 0) // can render first person body here
 		{
-			if (g_player_hud && pDemoRecords.empty() && !m_holder)
+			if (g_player_hud && pDemoRecords.empty() && !m_holder && !showActorBody)
 			{
 				g_player_hud->render_legs();
 			}
+
+            if (showActorBody)
+            {
+                inherited::renderable_Render();
+                CInventoryOwner::renderable_Render();
+            }
 
 			//if (fpBody) 
 			//	inherited::renderable_Render();
