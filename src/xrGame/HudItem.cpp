5d1dbd697c9213a84d40ade54135bf972b22fa10
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2026-03-10 02:25:32 +1000

    owner check in CHudItem::renderable_Render

--- src/xrGame/HudItem.cpp
+++ src/xrGame/HudItem.cpp
@@ -1,4 +1,4 @@
-﻿#include "stdafx.h"
+#include "stdafx.h"
 #include "HudItem.h"
 #include "physic_item.h"
 #include "actor.h"
@@ -131,10 +131,12 @@ void CHudItem::renderable_Render(IDSGraphManager* DM)
 		else if (object().H_Parent())
 		{
 			CInventoryOwner* owner = smart_cast<CInventoryOwner*>(object().H_Parent());
-			VERIFY(owner);
-			CInventoryItem* self = smart_cast<CInventoryItem*>(this);
-			if (owner->attached(self))
-				on_renderable_Render(DM);
+            if (owner)
+            {
+                CInventoryItem* self = smart_cast<CInventoryItem*>(this);
+                if (owner->attached(self))
+                    on_renderable_Render(DM);
+            }			
 		}
 	}
 }
