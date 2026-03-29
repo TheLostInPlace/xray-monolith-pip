86896cf8d31c0c878cb1a81c917ee6f0e55d13e6
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2026-03-29 16:40:33 +1000

    Revert "visual memory manager more safery p2"
    This reverts commit 60af2b7fdf89a944c8bd95c8aff8720eb7a17717.

--- src/xrGame/visual_memory_manager_inline.h
+++ src/xrGame/visual_memory_manager_inline.h
@@ -10,7 +10,7 @@
 
 IC xr_shared_ptr<CVisualMemoryManager::VISIBLES> CVisualMemoryManager::objects() const
 {
-    if (!this || !m_objectsShared)
+    if (!m_objectsShared)
         return nullptr;
 	return m_objectsShared;
 }
