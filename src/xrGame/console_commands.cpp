b99b08d20dd838eed739a9c45717795839fb0b36
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2026-04-15 15:44:28 +1000

    CTorch::Update * `r__optimize_torch` cvar to toggle * use GetPerceivedDist for more accuracy when looking through binocs or scopes or low fov

--- src/xrGame/console_commands.cpp
+++ src/xrGame/console_commands.cpp
@@ -183,6 +183,7 @@ extern float IK_CALC_DIST;
 extern float IK_CALC_SSA;
 extern float IK_ALWAYS_CALC_DIST;
 extern BOOL r_optimize_calculate_bones;
+extern BOOL r_optimize_torch;
 
 extern BOOL g_legs_enabled;
 extern float legs_fwd_offset;
@@ -2920,6 +2921,7 @@ void CCC_RegisterCommands()
 	CMD4(CCC_Float, "ik_calc_ssa", &IK_CALC_SSA, 0.001f, 0.02f);
 	CMD4(CCC_Float, "ik_always_calc_dist", &IK_ALWAYS_CALC_DIST, 10, 50);
 	CMD4(CCC_Integer, "r__optimize_calculate_bones", &r_optimize_calculate_bones, 0, 1);
+	CMD4(CCC_Integer, "r__optimize_torch", &r_optimize_torch, 0, 1);
 
 	CMD4(CCC_Integer, "g_progressive_stamina_cost", &progressiveStaminaCost, 0, 1);
 	CMD4(CCC_Integer, "g_npcs_look_at_actor", &NPCsLookAtActor, 0, 1);
